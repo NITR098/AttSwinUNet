828b9c61e8a657c9436fe41662440f7798b856e1
unknown <maralzar>
2022-11-13 22:54:09 +0330

    remove Synapse

--- networks/__init__.py
+++
@@ -1,9 +0,0 @@
-from .attention_swin_unet import SwinAttentionUnet
-from .swin_block import SwinTransformerBlock
-from .encoder import Encoder
-from .decoder import Decoder
-from .embedding import PatchEmbed
-from .partitioning import window_partition,window_reverse
-from .attention import WindowAttention
-from .swin_block import SwinTransformerBlock
-
